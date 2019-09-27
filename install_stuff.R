05c2317fe83a07a25c89d45c2d1eb5218ae2dd9d
Owen Chapman <ochapman@eng.ucsd.edu>
2019-09-26 20:18:24 -0700

    Update install_stuff.R

--- install_stuff.R
+++ install_stuff.R
@@ -1,7 +1,5 @@
-## try http:// if https:// URLs are not supported
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-BiocManager::install(ask=FALSE)
 BiocManager::install("conumee",ask=FALSE)
 BiocManager::install("minfi", ask=FALSE)
 BiocManager::install("biomaRt", ask=FALSE)
