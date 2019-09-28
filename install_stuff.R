614f8ba646e47a386e7cc5ab383125aa116517f8
edjuaro <edjuaro@gmail.com>
2019-09-27 17:23:08 -0700

    updating Dockerfile to use jupyter/r-notebook

--- install_stuff.R
+++ install_stuff.R
@@ -1,7 +1,11 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install("conumee",ask=FALSE)
+install.packages("BiocManager")
+BiocManager::install(c("GenomicRanges", "SummarizedExperiment","bumphunter","GenomeInfoDb","illuminaio","genefilter","GEOquery"), ask=FALSE)
 BiocManager::install("minfi", ask=FALSE)
+
+BiocManager::install(c("IlluminaHumanMethylation450kanno.ilmn12.hg19", "IlluminaHumanMethylation450kmanifest","IlluminaHumanMethylationEPICanno.ilm10b2.hg19","IlluminaHumanMethylationEPICmanifest","rtracklayer","genefilter","GEOquery"), ask=FALSE)
+BiocManager::install("conumee",ask=FALSE)
+
+BiocManager::install(c("XML", "RCurl","httr"), ask=FALSE)
 BiocManager::install("biomaRt", ask=FALSE)
 
 install.packages(c("optparse", "parallel", "foreach", "doParallel"))
